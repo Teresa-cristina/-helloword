14d7f7f75e51498afa89a999c29744fd11e4fc05
Teresa-cristina <tcrist.cristina952@gmail.com>
2024-10-15 11:54:10 -0300

    aula 15/10

---
+++ app01.py
@@ -0,0 +1,52 @@
+#Criar uma variável
+
+'''
+Esse comentário pode
+ter muitas
+e muitas 
+linhas
+'''
+
+"""
+Esse comentário pode
+ter muitas
+e muitas 
+linhas
+"""
+
+
+# Criar uma variável usando "snake case"
+cor_da_casa = 'azul'
+
+# Exibir as coisas
+print(cor_da_casa)
+
+#Criar uma variável usando "camelCase"
+corDaCasa ="amarela"
+
+# Exibe as coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(corDaCasa)
+
+
+# Primeiro nome
+first_name = "Cristina"
+
+# Último nome
+last_name = 'Medeiros'
+
+
+# Imprime meu nome
+print(first_name)
+print(last_name)
+
+# Imprime os dois nomes
+print(first_name, last_name)
+
+
+
+
+
+
+
